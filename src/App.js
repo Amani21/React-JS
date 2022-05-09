71da3b4b6581e104f452e6ad238a2e91a656416c
Amani <fallah.amani@gmail.com>
2022-05-09 14:45:11 +0100

    fisrt commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Titre from './components/Titre';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Formulaire from './components/Formulaire';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+         <Titre/>
+         <Formulaire/>
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Titre.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './formulaire.css'
+
+
+function titre() {
+  return (
+    <div className='tt'>
+        <h1> Bonjour, vous etes le bienvenue !  </h1>
+        <h5> Veuillez remplir ce formulaire </h5>
+    </div>
+  )
+}
+
+export default titre
